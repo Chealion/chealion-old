1b55bc5c9be46fe490b53db8469748cdc914e3da
Micheal Jones <micheal@mcjones.ca>
2008-10-26 00:49:24 -0600

    Revert "Merge branch 'master' into wpplug"
    This reverts commit 06df3e3a707597cf9bc77738c699fc74e5fbbe84.

--- Shell/parentlock.sh
+++ Shell/parentlock.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
-# Removes .parentlock files from all Thunderbird and Camino Profiles folders
-exec 2>/dev/console
-rm -f ~/Library/Thunderbird/Profiles/*.default/.parentlock
-rm -f ~/Library/Application\ Support/Thunderbird/Profiles/*.default/.parentlock
-rm -f ~/Library/Application\ Support/Camino/.parentlock
-
-/usr/bin/osascript <<-EOF
-	tell application "parentlock"
-	display dialog "Removed .parentlock files."
-	end tell
-EOF
+# Removes .parentlock files from all Thunderbird Profiles folders
+rm -f ~/Library/Thunderbird/Profiles/*.profile/.parentlock
+rm -f ~/Library/Application\ Support/Thunderbird/Profiles/*.profile/.parentlock
--- Shell/whatIsOpen.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# Micheal Jones
-# 10/20/08
-# What's open on other volumes?
-lsof -F n | grep '/Volumes/' | sed 's/n\/Volumes\///g'| sort -f
