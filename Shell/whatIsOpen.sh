409fdffb4b1041351a628de28b84897c3ab73e75
Micheal Jones <chealion@chealion.ca>
2009-02-10 16:40:01 -0700

    Fix up whatIsOpen to avoid false positives

--- Shell/whatIsOpen.sh
+++ Shell/whatIsOpen.sh
@@ -2,4 +2,7 @@
 # Micheal Jones
 # 10/20/08
 # What's open on other volumes?
-lsof -F n | grep '/Volumes/' | sed 's/n\/Volumes\///g'| sort -f
+# Updated: 2/10/09 - Fixed false positives from folders simply called Volumes eg. /private/Network/Servers/*/Volumes/
+#
+
+lsof -F n | grep '^n/Volumes/' | sed 's/n\/Volumes\///g' | sort -f
