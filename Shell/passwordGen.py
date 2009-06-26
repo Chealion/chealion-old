6b88f7a43012608e42dc7e5ff9448208743b9151
Micheal Jones <chealion@chealion.ca>
2009-06-26 12:29:05 -0600

    Basic working version of passwordGen

--- Shell/passwordGen.py
+++ Shell/passwordGen.py
@@ -20,14 +20,43 @@ hotel - uniform - Ampersand - eight - nine - JULIET - kilo - india - three - eig
 
 """
 
-__author__ = "Micheal Jones (michealj@joemedia.tv)"
+__author__ = "Micheal Jones (chealion@chealion.ca)"
 __version__ = "$Revision: 1.0 $"
-__date__ = "$Date: 2009/01/01 $"
+__date__ = "$Date: 2009/06/26 $"
 __copyright__ = "Copyright (c) 2009 Micheal Jones"
 __license__ = "BSD"
 
 import sys
 import os
+import random
+
+########################
+
+def usage(error):
+# Show Usage
+	print "Usage: passwordGen.py STUFF\n\n"
+	
+
+########################
+
+def createPassword():
+# Create a random password - modified from passwordGen.py
+	password = ""
+	phonetics = ""
+
+	#Create Dictionaries
+	sub_phoneticAlphabet = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "juliet", "kilo", "mike", "november", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "ALPHA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT", "GOLF", "HOTEL", "JULIET", "KILO", "MIKE", "NOVEMBER", "PAPA", "QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY", "XRAY", "YANKEE", "ZULU", "two", "three", "four", "five", "six", "seven", "eight", "nine", "Tilde", "At sign", "Hash", "Dollar sign", "Percent sign", "Caret", "Ampersand", "Asterisk", "Dash", "Underscore", "Period"]
+	sub_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "2", "3", "4", "5", "6", "7", "8", "9", "~", "@", "#", "$", "%", "^", "&", "*", "-", "_", "."]
+
+	for count in range(0,8):
+		#Choose a random character
+		character = random.randint(0, len(sub_alphabet) - 1)
+
+		password += sub_alphabet[character]
+		phonetics += sub_phoneticAlphabet[character] + " "
+
+	return password, phonetics
+
 
 ########################
 
@@ -35,10 +64,10 @@ def main(argv):
 	
 	#Need to use getopt
 	
-	phoneticAlphabet = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu"]
-	sub_phoneticAlphabet = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "juliet", "kilo", "mike", "november", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "two", "three", "four", "five", "six", "seven", "eight", "nine", "Tilde", "At sign", "Hash", "Dollar sign", "Percent sign", "Caret", "Ampersand", "Asterisk", "Left Parenthesis", "Right Parenthesis", "Dash", "Underscore", "Plus sign", "Equals sign", "Left curly bracket", "Right curly brakcet", "Left square bracket", "Right square bracket", "Vertical line", "Backslash", "Colon", "Semicolon", "Quote", "Double Quote", "Less than symbol", "Greater than symbol", "Period", "Comma"]
-	sub_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "2", "3", "4", "5", "6", "7", "8", "9", "~", "@", "#", "$", "%", "^", "&", "*", "(", ")", "\[", "\]", "-", "_", "+", "=", "{", "}", "|", "\\", ":", ";", "'", "\"", "<", ">", ".", "\,"]
-
+	password = createPassword()
+	phonetics = password[1]
+	password = password[0]
+	print "\nPassword:  " + password + "\nPhonetic:  " + phonetics + "\n"
 
 ########################
 
